5c88550a08411ed851e1992da966be4ca956f5c2
mockee <mockee@gmail.com>
2023-02-15 14:10:24 +0800

    [Devicons] Do some cleanup

--- after/plugin/web-devicons.rc.lua
+++ after/plugin/web-devicons.rc.lua
@@ -1,12 +1,6 @@
-local status, icons = pcall(require, 'nvim-web-devicons')
+local status, devicons = pcall(require, 'nvim-web-devicons')
 if (not status) then return end
 
-icons.setup {
-  -- your personnal icons can go here (to override)
-  -- DevIcon will be appended to `name`
-  override = {
-  },
-  -- globally enable default icons (default to false)
-  -- will get overriden by `get_icons` option
-  default = true
+devicons.setup {
+  default = true,
 }
