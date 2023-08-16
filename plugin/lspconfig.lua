92d371d1842b7c2929bdcbf4eb64694c674b3b31
mockee <mockee@gmail.com>
2023-08-16 17:21:09 +0800

    [Lspconfig] Update icon

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -99,7 +99,6 @@ nvim_lsp.lua_ls.setup {
         -- Get the language server to recognize the `vim` global
         globals = { 'vim' },
       },
-
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file('', true),
@@ -126,11 +125,11 @@ nvim_lsp.astro.setup {
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = '●' },
-  severity_sort = true,
-}
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = '\u{ea71}' },
+    severity_sort = true,
+  }
 )
 
 -- Diagnostic symbols in the sign column (gutter)
