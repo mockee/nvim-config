8f89a4d56d6a61a620a5bf8b292db347e297b917
mockee <mockee@gmail.com>
2023-08-16 17:41:12 +0800

    [Gitsign] Display current line blame

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -1,4 +1,9 @@
 local status, gitsigns = pcall(require, 'gitsigns')
 if (not status) then return end
 
-gitsigns.setup {}
+gitsigns.setup {
+  current_line_blame = true,
+  current_line_blame_opts = {
+    delay = 300
+  }
+}
